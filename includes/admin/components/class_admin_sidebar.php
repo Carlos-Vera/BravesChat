d086e37ab418470d34e6a0c1b8a8432fd045c70c
Carlos Vera <carlos@braveslab.com>
2026-02-24 19:28:34 +0100

    Update class_admin_sidebar.php
    Se agrega la sección al sidebar de Estadísticas con el botón en SVG.

--- includes/admin/components/class_admin_sidebar.php
+++ includes/admin/components/class_admin_sidebar.php
@@ -127,6 +127,13 @@ class Admin_Sidebar {
                 'page_slug' => 'braves-chat-gdpr',
                 'icon' => $this->get_icon_svg('gdpr'),
             ),
+            array(
+                'id'        => 'statistics',
+                'label'     => __('Estadísticas', 'braves-chat'),
+                'url'       => admin_url('admin.php?page=braves-chat-stats'),
+                'page_slug' => 'braves-chat-stats',
+                'icon'      => $this->get_icon_svg('statistics'),
+            ),
         );
 
         /**
@@ -189,6 +196,9 @@ class Admin_Sidebar {
             </svg>',
             'gdpr' => '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zM9 8V6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9z" fill="currentColor"/>
+            </svg>',
+            'statistics' => '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 9.2h3V19H5zM10.6 5h2.8v14h-2.8zm5.6 8H19v6h-2.8z" fill="currentColor"/>
             </svg>',
         );
 
